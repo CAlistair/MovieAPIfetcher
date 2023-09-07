bb6ef676edc520a825166b6e0a7d0d48626ead53
calistair <calistair@outlook.com>
2023-09-07 17:10:59 +0100

    fixing JS

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 //https://www.omdbapi.com/?apikey=31b6f6e5&s=
 
-var term = document.getElementById("search__box")
+const term = document.getElementById("search__box")
 
 async function findMovies(event){
-    const term = event.target.value;
+    const term = event.target.value
     search(term)
 }
 
 async function search(term){
-    const title = await fetch(`https://www.omdbapi.com/?apikey=31b6f6e5&s=${term}`)
-    const movie = await title.json();
-    console.log(movie)
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=31b6f6e5&s=${term}`);
+    const moviesData = await movies.json();
+    console.log(moviesData)
 }
-
-
 
 search()
