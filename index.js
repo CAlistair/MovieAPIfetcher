d48e2c98d83da5e203119b0b4b1376a9748c4a68
calistair <calistair@outlook.com>
2023-09-07 17:44:54 +0100

    fixed inital issue new one came up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //https://www.omdbapi.com/?apikey=31b6f6e5&s=
 
-const term = document.getElementById("search__box")
+const term = document.getElementById("search-input").addEventListener("input", findMovies)
 
 async function findMovies(event){
     const term = event.target.value
@@ -8,9 +8,8 @@ async function findMovies(event){
 }
 
 async function search(term){
-    const movies = await fetch(`https://www.omdbapi.com/?apikey=31b6f6e5&s=${term}`);
+    const movies = await fetch(`https://www.omdbapi.com/?s=${term}&apikey=31b6f6e5`);
     const moviesData = await movies.json();
     console.log(moviesData)
 }
 
-search()
