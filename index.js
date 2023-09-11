b24ec7a149547aac87c2a0d11d920842cd9d77c9
calistair <calistair@outlook.com>
2023-09-11 14:19:51 +0100

    Finished JS code to use API retriever and implement it into HTML, styled correctly and made compatible with different devices and also added a footer which stays at the bottom when content is not loaded, next is to add a loading phase

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //https://www.omdbapi.com/?apikey=31b6f6e5&s=
-
 const term = document.getElementById(`input-box`);
+const movieListE1 = document.querySelector(" .movies__container")
+
+let allMovies = [""]
 
 async function findMovies(event){
     const term = event.target.value
@@ -11,6 +13,24 @@ async function search(term){
     const movies = await fetch(`https://www.omdbapi.com/?s=${term}&apikey=31b6f6e5`);
     const moviesData = await movies.json();
     console.log(moviesData)
+    
+    if (moviesData.Search){
+        allMovies = moviesData.Search;
+        displayMovies(allMovies.slice(0,6))
+    }
 }
 
 
+function displayMovies(movies){
+    setTimeout(() => {
+        movieListE1.innerHTML = movies.map(movie =>
+     `<div class="movie">
+    <figure>
+        <img class="movie__img" src="${movie.Poster}" alt="">
+    </figure>
+    <h1>${movie.Title}</h1>
+    <h2>Year: ${movie.Year}</h2>
+</div>`
+).join("");
+},1000)
+}
