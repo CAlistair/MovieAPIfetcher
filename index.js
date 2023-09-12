ef27e65748f6d76e441dff4e51a8e4a044ee869f
calistair <calistair@outlook.com>
2023-09-12 12:34:29 +0100

    Added loading state onto API fetcher

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //https://www.omdbapi.com/?apikey=31b6f6e5&s=
 const term = document.getElementById(`input-box`);
 const movieListE1 = document.querySelector(" .movies__container")
+const movieWrapper = document.querySelector(" .movie--wrap")
 
 let allMovies = [""]
 
@@ -10,18 +11,18 @@ async function findMovies(event){
 }
 
 async function search(term){
+    movieWrapper.classList.remove(`movies__loading-none`);
     const movies = await fetch(`https://www.omdbapi.com/?s=${term}&apikey=31b6f6e5`);
     const moviesData = await movies.json();
-    console.log(moviesData)
     
     if (moviesData.Search){
         allMovies = moviesData.Search;
         displayMovies(allMovies.slice(0,6))
     }
     else {
-        console.log("error")
         movieListE1.innerHTML = 
         "<h3>No movies found</h3>";
+        movieWrapper.classList.add(`movies__loading-none`);
     }
 }
 
@@ -37,5 +38,6 @@ function displayMovies(movies){
     <h2>Year: ${movie.Year}</h2>
 </div>`
 ).join("");
+movieWrapper.classList.add(`movies__loading-none`);
 },1000)
 }
